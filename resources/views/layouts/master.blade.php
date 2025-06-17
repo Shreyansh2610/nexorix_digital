7eddeeeec7db2a37601d2d2dda4b38f86fc0e224
Shreyansh2610 <shreyansh7874056628@gmail.com>
2025-06-17 22:03:58 +0530

    SEO avoid

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -8,6 +8,7 @@
     <meta name="viewport" content="width=device-width">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="robots" content="noindex, nofollow" />
     <title>@yield('title') - {{ config('app.name') }}</title>
     {{-- <link rel="apple-touch-icon" href="{{ asset('images/ico/apple-icon-120.png') }}"> --}}
     <link rel="shortcut icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
