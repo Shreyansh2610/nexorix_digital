cca79e30d37573d61935e10aedc17cfcbb54cd45
Shreyansh2610 <shreyansh7874056628@gmail.com>
2025-06-18 22:23:22 +0530

    about us

--- resources/views/company/aboutus.blade.php
+++ resources/views/company/aboutus.blade.php
@@ -10,7 +10,88 @@
 @section('page-styles')
 @endsection
 @section('content')
+    <div class="">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 py-5">
+                    <div class="row g-lg-4 gy-5 my-5">
+                        <div class="col-lg-6">
+                            <div class="magnetic-wrap">
+                                <div class="case-img magnetic-item" style="">
+                                    <img class="img-fluid" src="assets/img/inner-pages/case-study-img-02.png" alt="">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-6 d-flex align-items-center">
+                            <div class="case-content">
+                                {{-- <h2>Overview</h2> --}}
+                                <p class="text-white">At {{ env('APP_NAME') }}, each of us works towards one goal –
+                                    Delivering
+                                    the best
+                                    Digital Marketing Services. We are a technology-driven digital marketing company. We
+                                    know
+                                    what a newly formed company needs. We do it efficiently by developing stunning websites
+                                    and
+                                    a planned digital marketing strategy. We are your extended marketing team always on our
+                                    toes
+                                    to deliver high-quality digital identities.</p>
+                                <p class="text-white">{{ env('APP_NAME') }} powerful team is a blend of carefully chosen
+                                    experts
+                                    from website
+                                    development and
+                                    digital marketing sectors. Each one nurture other so that all of us learn and grow in
+                                    the
+                                    process while we continue to deliver you the best of our services. We are transforming
+                                    one
+                                    business every day, helping them smoothly sync with the ever-changing digital marketing
+                                    world. We help brands remodel for continuous growth.</p>
+                            </div>
+                        </div>
 
+                    </div>
+                    <div class="row g-lg-4 gy-5 my-5">
+                        <div class="col-lg-6 d-flex align-items-center">
+                            <div class="case-content">
+                                <h2 class="text-center text-success-theme">Our Vision</h2>
+                                <p class="text-white">We've built a great bond with clients in India, delivering hassle-free
+                                    website development and SEO Services in India. In the coming years, we aim to go global
+                                    to
+                                    take our expertise to brands in different countries.</p>
+                            </div>
+                        </div>
+                        <div class="col-lg-6">
+                            <div class="magnetic-wrap">
+                                <div class="case-img magnetic-item" style="">
+                                    <img class="img-fluid" src="assets/img/inner-pages/case-study-img-02.png"
+                                        alt="">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row g-lg-4 gy-5 my-5">
+                        <div class="col-lg-6">
+                            <div class="magnetic-wrap">
+                                <div class="case-img magnetic-item" style="">
+                                    <img class="img-fluid" src="assets/img/inner-pages/case-study-img-02.png"
+                                        alt="">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-6 d-flex align-items-center">
+                            <div class="case-content">
+                                <h2 class="text-center text-success-theme">Our Mission</h2>
+                                <p class="text-white">We aim to provide innovative brand solutions, lead with a purpose,
+                                    remain
+                                    relevant and make a difference. Every delivery at {{ env('APP_NAME') }} is carefully
+                                    supervised so that
+                                    every brand’s message is delivered as intended.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
 
 {{-- vendor scripts --}}
--- resources/views/panels/breadcrumbs.blade.php
+++ resources/views/panels/breadcrumbs.blade.php
@@ -1,14 +1,14 @@
 <!-- Start breadcrumbs section -->
 @isset($breadcrumbs)
     <section class="breadcrumbs">
-        <div class="breadcrumb-sm-images">
+        {{-- <div class="breadcrumb-sm-images">
             <div class="inner-banner-1 magnetic-item">
                 <img src="assets/img/inner-pages/inner-banner-1.png" alt="">
             </div>
             <div class="inner-banner-2 magnetic-item">
                 <img src="assets/img/inner-pages/inner-banner-2.png" alt="">
             </div>
-        </div>
+        </div> --}}
         <div class="container">
             <div class="row">
                 <div class="col-12">
